6ae1ed4fc9db77f2f34f321a2c8358ec9325cf17
Alfredo Patricius Tarigan <alfredoptarigan@students.usu.ac.id>
2021-08-17 22:01:07 +0700

    User Model & Validation

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const connectDatabase = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDatabase();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 	res.send("API Running");
 });
+
+// Define Route
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
