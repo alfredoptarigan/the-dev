5b03b12192a14e7a17236cb138b490c9452b783e
Alfredo Patricius Tarigan <alfredoptarigan@students.usu.ac.id>
2021-08-17 22:51:26 +0700

    Wip register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
+const User = require("../../models/User");
+
+// @route   POST api/users
 // @desc    Register User
 // @access  Public
 
@@ -15,13 +19,48 @@ const validationRegister = [
 	).isLength({ min: 6 }),
 ];
 
-router.post("/", validationRegister, (req, res) => {
+router.post("/", validationRegister, async (req, res) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() });
 	}
 
-	res.send("User route");
+	const { name, email, password } = req.body;
+
+	try {
+		// Check if the users exists
+		let user = await User.findOne({ email });
+		if (user) {
+			return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+		}
+		// Get users gravatar
+		const avatar = gravatar.url(email, {
+			s: "200",
+			r: "pg",
+			d: "mm",
+		});
+
+		// Create User
+		user = new User({
+			name,
+			email,
+			avatar,
+			password,
+		});
+
+		const salt = await bcrypt.genSalt(10);
+
+		user.password = await bcrypt.hash(password, salt);
+
+		await user.save();
+
+		// Return JWT
+
+		res.send("User successfully register !");
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
 });
 
 module.exports = router;
