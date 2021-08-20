c59c949d47907a9dc174198121144fe48073a415
Alfredo Patricius Tarigan <alfredoptarigan@students.usu.ac.id>
2021-08-20 22:22:16 +0700

    Implement Delete Experiences

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const User = require("../../models/User");
 
 const {
 	createProfileValidation,
+	experienceProfileValidation,
 } = require("../../validation/ProfileValidation");
 
 // @route   GET api/profile/me
@@ -151,6 +152,70 @@ router.delete("/", auth, async (req, res) => {
 		await User.findOneAndRemove({ _id: req.user.id });
 
 		res.json({ msg: "User deleted" });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put(
+	"/experience",
+	[auth, experienceProfileValidation],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.status(400).json({ errors: errors.array });
+
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.experiences.unshift(newExp);
+
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		// Get remove index
+		const removeIndex = profile.experiences
+			.map((item) => item.id)
+			.indexOf(req.params.exp_id);
+
+		profile.experiences.splice(removeIndex, 1);
+
+		await profile.save();
+
+		res.json(profile);
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send("Server Error");
--- validation/ProfileValidation.js
+++ validation/ProfileValidation.js
@@ -5,6 +5,14 @@ const createProfileValidation = [
 	check("skills", "Skills is required").not().isEmpty(),
 ];
 
+const experienceProfileValidation = [
+	check("title", "Title is required").not().isEmpty(),
+	check("company", "Title is required").not().isEmpty(),
+	check("company", "Title is required").not().isEmpty(),
+	check("from", "From date is required").not().isEmpty(),
+];
+
 module.exports = {
 	createProfileValidation,
+	experienceProfileValidation,
 };
