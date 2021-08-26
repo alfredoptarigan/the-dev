2d5e5716d663c6ac20c332bcca1e2ee080edd394
Alfredo Patricius Tarigan <alfredoptarigan@students.usu.ac.id>
2021-08-27 00:56:19 +0700

    Wip

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,94 @@ router.post("/", [auth, validationPost], async (req, res) => {
 	}
 });
 
+// @route   GET api/posts
+// @desc    Get All Posts
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   GET api/posts/:id
+// @desc    Get Specific Post by ID
+// @access  Private
+
+router.get("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.json(post);
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete Post
+// @access  Private
+
+router.delete("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check user
+
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		await post.remove();
+
+		res.json({ msg: "Post removed" });
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like Post
+// @access  Private
+
+router.put("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post already been liked
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id).length >
+			0
+		) {
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
 module.exports = router;
--- validation/PostValidation.js
+++ validation/PostValidation.js
@@ -2,6 +2,20 @@ const { check, validationResult } = require("express-validator");
 
 const validationPost = [check("text", "Text is required").not().isEmpty()];
 
+const checkPost = (post) => {
+	if (!post) {
+		return res.status(404).json({ msg: "Post not found" });
+	}
+};
+
+const errObjectId = (err) => {
+	if (err === "ObjectId") {
+		return res.status(404).json({ msg: "Post not found" });
+	}
+};
+
 module.exports = {
 	validationPost,
+	checkPost,
+	errObjectId,
 };
